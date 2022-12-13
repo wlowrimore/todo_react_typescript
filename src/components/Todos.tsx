426fda80ab59c5ea9cf635106faaf0980b0dad69
William Lowrimore <wlowrimore@gmail.com>
2022-12-13 11:42:26 -0600

    adds "adding todo and removing todo"

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-import Todo from '../models/todo';
+import TodoItem from "./TodoItem";
+import Todo from "../models/todo";
+import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
